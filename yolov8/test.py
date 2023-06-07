666c04a5178a5093844e3d484e14396399940d2b
wooyeolBaek <dwybaek7@gmail.com>
2023-06-07 13:14:25 +0000

    fix: test.py 정리

--- yolov8/test.py
+++ yolov8/test.py
@@ -19,8 +19,10 @@ import shutil
 from tqdm.auto import tqdm
 from argparse import ArgumentParser
 
+from pytz import timezone
+from datetime import datetime
+
 from ultralytics import YOLO
-from IPython.display import clear_output
 
 def parse_args():
     parser = ArgumentParser()
@@ -29,10 +31,17 @@ def parse_args():
     parser.add_argument('--data', type=str, default='./data/yolo/custom.yaml')
     parser.add_argument('--seed', type=int, default=2023)
     parser.add_argument('--batch', type=int, default=16)
-    parser.add_argument('--model_dir', type=str, default='./object_detection_for_synthetic_data/exp/weights/best.pt')
+    parser.add_argument('--model_dir', type=str, default='./yolov8x/230606_141148/weights/epoch260.pt')
+    parser.add_argument('--project_name', type=str, default='yolov8x')   
+
+    parser.add_argument('--test_dir', type=str, default='../open/test/*.png')
+    # parser.add_argument('--test_dir', type=str, default='../open/yolo/valid0/*.png')
+    
 
     parser.add_argument('--imgsz_w', type=int, default=1920)
     parser.add_argument('--imgsz_h', type=int, default=1080)
+    # parser.add_argument('--imgsz_w', type=int, default=1024)
+    # parser.add_argument('--imgsz_h', type=int, default=555)
     parser.add_argument('--conf', type=float, default=0.6) # 0.25
     parser.add_argument('--iou', type=float, default=0.4) # 0.7
     parser.add_argument('--half', type=bool, default=False)
@@ -50,11 +59,6 @@ def parse_args():
     parser.add_argument('--exist_ok', type=bool, default=True)
     parser.add_argument('--verbose', type=bool, default=False)
 
-    # --wandb configs
-    parser.add_argument("--wandb_project", type=str, default="object_detection_for_synthetic_data")
-    parser.add_argument("--num_of_classes", type=int, default=34)
-    parser.add_argument("--wandb_name", type=str, default="exp")
-
     args = parser.parse_args()
 
 
@@ -69,47 +73,52 @@ def seed_everything(seed):
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = True
 
+
+classes = [
+    'chevrolet_malibu_sedan_2012_2016',
+    'chevrolet_malibu_sedan_2017_2019',
+    'chevrolet_spark_hatchback_2016_2021',
+    'chevrolet_trailblazer_suv_2021_',
+    'chevrolet_trax_suv_2017_2019',
+    'genesis_g80_sedan_2016_2020',
+    'genesis_g80_sedan_2021_',
+    'genesis_gv80_suv_2020_',
+    'hyundai_avante_sedan_2011_2015',
+    'hyundai_avante_sedan_2020_',
+    'hyundai_grandeur_sedan_2011_2016',
+    'hyundai_grandstarex_van_2018_2020',
+    'hyundai_ioniq_hatchback_2016_2019',
+    'hyundai_sonata_sedan_2004_2009',
+    'hyundai_sonata_sedan_2010_2014',
+    'hyundai_sonata_sedan_2019_2020',
+    'kia_carnival_van_2015_2020',
+    'kia_carnival_van_2021_',
+    'kia_k5_sedan_2010_2015',
+    'kia_k5_sedan_2020_',
+    'kia_k7_sedan_2016_2020',
+    'kia_mohave_suv_2020_',
+    'kia_morning_hatchback_2004_2010',
+    'kia_morning_hatchback_2011_2016',
+    'kia_ray_hatchback_2012_2017',
+    'kia_sorrento_suv_2015_2019',
+    'kia_sorrento_suv_2020_',
+    'kia_soul_suv_2014_2018',
+    'kia_sportage_suv_2016_2020',
+    'kia_stonic_suv_2017_2019',
+    'renault_sm3_sedan_2015_2018',
+    'renault_xm3_suv_2020_',
+    'ssangyong_korando_suv_2019_2020',
+    'ssangyong_tivoli_suv_2016_2020'
+]
+
+
 # 필요한 dir들 생성
 def initialize():
     if os.path.exists("../open/yolo"):
         shutil.rmtree("../open/yolo")
         
-    # if not os.path.exists("../open/yolo/valid"):
-    #     os.makedirs("../open/yolo/valid")
-        
     if not os.path.exists("../open/yolo/test"):
         os.makedirs("../open/yolo/test")    
-        
-    if not os.path.exists("./results"):
-        os.makedirs("./results")
-
-# 지정한 폴더의 png, txt 파일들로 yolo dataset 폴더를 만들어줌
-def make_yolo_dataset(image_paths, txt_paths, type="train"):
-    for image_path, txt_path in tqdm(zip(image_paths, txt_paths if not type == "test" else image_paths), total=len(image_paths)):
-        source_image = cv2.imread(image_path, cv2.IMREAD_COLOR)        
-        image_height, image_width, _ = source_image.shape
-        
-        target_image_path = f"../open/yolo/{type}/{os.path.basename(image_path)}"
-        cv2.imwrite(target_image_path, source_image)
-        
-        if type == "test":
-            continue
-        
-        with open(txt_path, "r") as reader:
-            yolo_labels = []
-            for line in reader.readlines():
-                line = list(map(float, line.strip().split(" ")))
-                class_name = int(line[0])
-                x_min, y_min = float(min(line[5], line[7])), float(min(line[6], line[8]))
-                x_max, y_max = float(max(line[1], line[3])), float(max(line[2], line[4]))
-                x, y = float(((x_min + x_max) / 2) / image_width), float(((y_min + y_max) / 2) / image_height)
-                w, h = abs(x_max - x_min) / image_width, abs(y_max - y_min) / image_height
-                yolo_labels.append(f"{class_name} {x} {y} {w} {h}")
-            
-        target_label_txt = f"../open/yolo/{type}/{os.path.basename(txt_path)}"
-        with open(target_label_txt, "w") as writer:
-            for yolo_label in yolo_labels:
-                writer.write(f"{yolo_label}\n")
 
 def get_test_image_paths(test_image_paths, batch):
     for i in range(0, len(test_image_paths), batch):
@@ -136,10 +145,12 @@ if __name__ == '__main__':
     # make_yolo_dataset(sorted(glob("../open/test/*.png")), None, "test")
 
     model = YOLO(args.model_dir)
-    test_image_paths = glob("../open/yolo/test/*.png")
-    for i, image in tqdm(enumerate(get_test_image_paths(test_image_paths, args.batch)), total=int(len(test_image_paths)/args.batch)):
+    test_image_paths = glob(args.test_dir)
+    print(f'png 개수 = {len(test_image_paths)}')
+    name = datetime.now(timezone("Asia/Seoul")).strftime("%y%m%d_%H%M%S")
+    for i, images in tqdm(enumerate(get_test_image_paths(test_image_paths, args.batch)), total=int(len(test_image_paths)/args.batch)):
         model.predict(
-            source=image,
+            source=images,
             imgsz=(args.imgsz_w, args.imgsz_h),
             conf=args.conf,
             iou=args.iou,
@@ -156,23 +167,29 @@ if __name__ == '__main__':
             agnostic_nms=args.agnostic_nms,
             exist_ok=args.exist_ok, 
             verbose=args.verbose,
-            project=args.wandb_project,
-            name=args.name,
+            
+            project=args.project_name,
+            name=name,
         )
-        if i % 5 == 0:
-            clear_output(wait=True)
 
     
-    infer_txts = glob(f"{args.wandb_project}/predict/labels/*.txt")
+    infer_txts = glob(f"{args.project_name}/{name}/labels/*.txt")
+    print(f'txt 개수 = {len(infer_txts)}')
+
+    # test_images = set([image_path.split('/')[-1].replace('.txt','') for image_path in test_image_paths])
+    # test_annos = set([txt_path.split('/')[-1].replace('.png', '') for txt_path in infer_txts])
+    # print(f'png - txt = {len(test_images - test_annos)}개: {test_images - test_annos}')
 
     results = []
     for infer_txt in tqdm(infer_txts):
         base_file_name = infer_txt.split("/")[-1].split(".")[0]
-        imgage_height, imgage_width = cv2.imread(f"../open/yolo/test/{base_file_name}.png").shape[:2]        
+        # image_height, image_width = cv2.imread(f"../open/test/{base_file_name}.png").shape[:2]
+        image_height, image_width = 1080, 1920
         with open(infer_txt, "r") as reader:        
             lines = reader.readlines()        
             for line in lines:
-                results.append(yolo_to_labelme(line, imgage_width, imgage_height, infer_txt))
+                results.append(yolo_to_labelme(line, image_width, image_height, infer_txt))
 
     df_submission = pd.DataFrame(data=results, columns=["file_name", "class_id", "confidence", "point1_x", "point1_y", "point2_x", "point2_y", "point3_x", "point3_y", "point4_x", "point4_y"])
-    df_submission.to_csv(f"./results/{args.wandb_project}.csv", index=False)
+    df_submission.to_csv(f"{args.model_dir.replace('.pt','.csv')}", index=False)
+    
